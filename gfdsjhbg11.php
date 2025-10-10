c441e1d9c89c5cc2714a0ad83dea77b7f3a8534e
Student <Student@ksipt.ru>
2025-10-10 15:04:17 +0300

    Vladimir

--- gfdsjhbg11.php
+++ gfdsjhbg11.php
@@ -1,5 +1,13 @@
 <?php
-$str = 'Привет!';
+$str = 'Hello!';
+for($i=0; $i<mb_strlen($str); $i++){
+    echo $str[$i], '<br>';
+}
 echo $str, '<br>';
 echo strlen($str);
+?>
+
+<?php
+echo '<h1>Задание 1</h1>';
+
 ?>
